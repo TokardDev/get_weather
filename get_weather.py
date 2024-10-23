9c5a1374dbb5858aee03ae28ae909d3ab9519bee
Tokard <58886109+TokardDev@users.noreply.github.com>
2024-10-23 16:07:46 +0200

    Update get_weather.py

--- get_weather.py
+++ get_weather.py
@@ -16,7 +16,7 @@ class Measurements:
 
     def __init__(self, town, country):
         # store every date, for each date store all the measurements
-        self._forecast = {}
+        self._forecast = {}  # key : date, value : list of measurements
         self.forecast_location = f"{town}({country})"
         self.forecast_min_temp = 0
         self.forecast_max_temp = 0
@@ -29,7 +29,7 @@ class Measurements:
             date (str): The date of the measurement.
             temp (float): The temperature of the measurement.
         """
-        if date in self._forecast:
+        if date in self._forecast:  # if the date already exists
             self._forecast[date].append(temp)
         else:
             self._forecast[date] = [temp]
@@ -41,6 +41,7 @@ class Measurements:
         Returns:
             float: The maximum temperature of the forecast.
         """
+        # get the max temp for each day, and then the global max temp
         return max([max(temps) for temps in self._forecast.values()])
 
     @property
@@ -50,6 +51,7 @@ class Measurements:
         Returns:
             float: The minimum temperature of the forecast.
         """
+        # same as above but for min temp
         return min([min(temps) for temps in self._forecast.values()])
 
     def to_json(self):
@@ -58,7 +60,7 @@ class Measurements:
         Returns:
             str: The JSON string of the forecast.
         """
-        self.forecast_min_temp = self.min_temp
+        self.forecast_min_temp = self.min_temp  # get the max and min
         self.forecast_max_temp = self.max_temp
 
         # skip the current day
@@ -79,23 +81,8 @@ class Measurements:
         }, indent=4)
 
 
-def clear_files():
-    try:
-        for file in os.listdir("out"):
-            os.remove(f"out/{file}")
-        logger.success("out directory cleared")
-    except FileNotFoundError:
-        logger.error("out directory not found")
-    try:
-        for file in os.listdir("logs"):
-            os.remove(f"logs/{file}")
-        logger.success("logs directory cleared")
-    except FileNotFoundError:
-        logger.error("logs directory not found")
-
-
 @click.command()
-@click.argument('city', nargs=1)
+@click.argument('city', nargs=1)  # add cli params
 @click.argument('country', nargs=1)
 def get_weather(city, country):
     """
@@ -103,7 +90,7 @@ def get_weather(city, country):
     Args:\n
         city (str): The name of the city.\n
         country (str): The country code of the city.\n
-    """
+    """  # I'm using \n to make the --help message more readable
 
     log_file = (
         f"logs/{city}({country})_"
@@ -126,37 +113,22 @@ def get_weather(city, country):
     url = f"{BASE_URL}&q={city},{country}&units=metric"
     response = requests.get(url)
     data = response.json()
-    # si le code de la reponse est 200 :
-    if response.status_code == 200:
+    if response.status_code == 200:  # if correctly fetched
         logger.success(f"Data fetched successfully for {city}({country})")
-        for measurement in data["list"]:
-            date = measurement["dt_txt"].split(" ")[0]  # get the date
-            temp = measurement['main']['temp']
+        for mesurement in data["list"]:
+            date = mesurement["dt_txt"].split(" ")[0]  # get the date
+            temp = mesurement['main']['temp']
             measure.add_temp(date, temp)
 
-        json_data = measure.to_json()  # convertis la classe en json
-        try:
-            json_name = f'out/{city}({country})_{datetime.date.today()}.json'
-            if os.path.exists(json_name):
-                logger.warning("File already exists, it will be overwritten")
-            with open(json_name, 'w') as f:
-                f.write(json_data)
-            logger.success(f"Data saved successfully for {city}({country})")
-        except FileNotFoundError:
-            try:
-                os.mkdir("out")
-                logger.warning("/out directory created")
-                today = datetime.date.today()
-                with open(f'out/{city}({country})_{today}.json',
-                          'w') as f:
-                    f.write(json_data)
-                logger.success(f"Data saved successfully for {city}")
-            except Exception as e:
-                logger.error(f"Failed to save data for {city}({country})")
-                logger.error(e)
-        except Exception as e:
-            logger.error(f"Failed to save data for {city}({country})")
-            logger.error(e)
+        json_data = measure.to_json()  # convert class to json
+        today = datetime.date.today()
+        os.makedirs("out", exist_ok=True)
+        json_name = f'out/{city}({country})_{today}.json'
+        if os.path.exists(json_name):  # check if the file already exists
+            logger.warning("File already exists, overwriting...")
+        with open(json_name, 'w') as f:
+            f.write(json_data)
+        logger.success(f"Data saved successfully in /out/{json_name}")
     else:
         logger.error(f"Failed to fetch data for {city}({country})")
         # log the error message (city not found, wrong api key, etc)
@@ -165,4 +137,11 @@ def get_weather(city, country):
 
 
 if __name__ == '__main__':
+    print("""\033[94m
+     ▄▄   ▄▄▄  ▄▄▄▄▄    ▄▄▌ ▐ ▄▌ ▄▄▄    ▄▄▄  ▄▄▄▄▄ ▄  ▄▄ ▄▄▄  ▄▄▄
+    ▐█ ▀  ▀▄ ▀  ██      ██  █▌▐█ ▀▄ ▀  ▐█ ▀█   █   ██ ▐█ ▀▄ ▀ ▀▄ █
+    ▄█ ▀█▄▐▀▀ ▄ ▐█      ██ ▐█▐▐▌ ▐▀▀ ▄ ▄█▀▀█   █   ██▀▀█ ▐▀▀ ▄▐▀▀▄ 
+    ▐█▄ ▐█▐█▄▄▌ ▐█▌     ▐█▌██▐█▌ ▐█▄▄▌ ▐█  ▐▌  █▌  ██ ▐█ ▐█▄▄▌▐█ █▌
+     ▀▀▀▀  ▀▀▀  ▀▀▀ ▄▄▄▄ ▀▀▀▀ ▀   ▀▀▀   ▀  ▀  ▀▀▀  ▀▀  ▀  ▀▀▀  ▀  ▀
+    """)
     get_weather()
